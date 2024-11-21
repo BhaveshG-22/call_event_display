0dff4a4f15197ca185bc4d82b2097489c13687b1
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-11-21 00:02:17 -0500

    modified:   src/components/RequestsDisplay.js

--- src/components/RequestsDisplay.js
+++ src/components/RequestsDisplay.js
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 const RequestsDisplay = ({ socket }) => {
+  console.log("In RequesDisplay component");
   const [requests, setRequests] = useState([]);
+  const [wsConnected, setWSConnected] = useState(false);
+
+  const PRIVATE_WS_ROOM = "123abc";
+  socket.emit("join", PRIVATE_WS_ROOM);
+
+  useEffect(() => {
+    socket.on("wsConnected", (boolean) => {
+      setWSConnected(boolean);
+      console.log("Connection ESTABLISHED SUCCESSFULLY!!!!");
+    });
+  }, [socket]);
 
   useEffect(() => {
     socket.on("requestResponse", (data) => {
@@ -12,15 +24,17 @@ const RequestsDisplay = ({ socket }) => {
     });
   }, [socket]);
 
-  return (
+  return !wsConnected ? (
+    <h1>Connecting to server!!!!</h1>
+  ) : (
     <div>
       <h1>RequestsDisplay</h1>
       {requests.map((request, index) => {
         console.log(requests);
         return (
           <h1 key={index}>
-            CALL_RECEIVED_ON: {request.CALL_RECEIVED_ON}
-            Restaurant_Name: {request.Restaurant_Name}
+            CALL_RECEIVED_ON: {request.CALL_RECEIVED_ON} <br />
+            Restaurant_Name: {request.Restaurant_Name} <br />
             Customer_ph_no: {request.Customer_ph_no}
           </h1>
         );
